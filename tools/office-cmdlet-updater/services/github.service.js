4e4679d27eed1fac827b884a992e48e9ad09dbcc
Yurij Bogdan <y2.bogdan@itransition.com>
2018-12-01 21:58:54 +0300

    Add github service

---
+++ tools/office-cmdlet-updater/services/github.service.js
@@ -0,0 +1,19 @@
+const { client } = require('octonode');
+
+class GithubService {
+	constructor(config) {
+		this.config = config;
+
+		const token = config.get('');
+
+		this.client = client(token);
+	}
+
+	openRepository({ repositoryName }) {
+		return this.client.repo(repositoryName);
+	}
+
+	createPullRequest({ repository, title, body, head, base, cb }) {
+		repository.pr({ title, body, head, base }, cb);
+	}
+}
