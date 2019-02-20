54d4258443d1c623726c58145aa81d47a00cf18c
Pavel Karpovich <pavel.karpovich@outlook.com>
2019-02-20 16:37:25 +0300

    Remove default error color

--- tools/office-cmdlet-updater/helpers/colors.js
+++ tools/office-cmdlet-updater/helpers/colors.js
@@ -1,6 +1,11 @@
 const chalk = require('chalk');
 
-const errorColor = chalk.redBright.bgBlack;
+const errorColor = ({ message }) => {
+	// remove default error color
+	const processedMsg = message.replace(/\[31m/g, '').replace(/\[39m/g, '');
+
+	return chalk.redBright.bgBlack(processedMsg);
+};
 
 module.exports = {
 	errorColor
